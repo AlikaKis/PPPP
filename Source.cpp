b3e79cf69db3ab7e7e09b10031f780796d88de80
AlikaKis <42034121+AlikaKis@users.noreply.github.com>
2021-04-13 18:49:33 +0300

    Lab2 done
    Unit tests for source program

--- Source.cpp
+++ Source.cpp
@@ -1,354 +1,126 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <cmath>
+#include <string>
+
 using namespace std;
 
-//Function for hand filling matrix
-int** InputMatrixOnHand(int** Matrix, int rows, int columns)
+
+
+string Program(double a, double b, double c, double d, double e, double f)
 {
-	for (int i = 0; i < rows; i++)
-		for (int j = 0; j < columns; j++)
-			cin >> Matrix[i][j];
-	return Matrix;
-}
+	string result;
 
-//Function for random filling matrix
-int** RandomInputMatrix(int** Matrix, int rows, int columns)
-{
-	for (int i = 0; i < rows; i++)
-		for (int j = 0; j < columns; j++)
-			Matrix[i][j] = rand() % 10;
-	return Matrix;
-}
-
-//Function for print matrix
-void MatrixOutput(int** Matrix, int rows, int columns)
-{
-	for (int i = 0; i < rows; i++)
+	if ((a == 0) && (b == 0) && (c == 0) && (d == 0) && (e == 0) && (f == 0))
 	{
-		for (int j = 0; j < columns; j++)
-			cout << Matrix[i][j] << " ";
-		cout << endl;
+		result = "5";
 	}
-}
-
-//Function for decomposition matrix to submatrix
-void DecompositiontoSubMatrix(int** subMatrix1, int** subMatrix2, int** subMatrix3, int** subMatrix4, int** ReductionMatrix, int length_required_matrix) {
-	for (int i = 0; i < length_required_matrix / 2; i++)
+	else if ((a * d - c * b != 0) && ((e * d - b * f != 0) || (a * f - c * e != 0)))
 	{
-		subMatrix1[i] = new int[length_required_matrix / 2];
-		for (int j = 0; j < length_required_matrix / 2; j++)
-			subMatrix1[i][j] = ReductionMatrix[i][j];
+		double y = (a * f - c * e) / (a * d - c * b);
+		double x = (d * e - b * f) / (d * a - b * c);
+		result = "2 " + (to_string(x)) + " " + to_string(y);
 	}
-	for (int i = 0; i < length_required_matrix / 2; i++)
+	else if (((a * d - c * b == 0) && ((e * d - b * f != 0) || (a * f - c * e != 0))) ||
+		(a == 0 && c == 0 && e / b != f / d) ||
+		(b == 0 && d == 0 && e / a != f / c) ||
+		(a == 0 && b == 0 && c == 0 && d == 0 && (e / f > 0)))
 	{
-		subMatrix2[i] = new int[length_required_matrix / 2];
-		for (int j = 0; j < length_required_matrix / 2; j++)
-			subMatrix2[i][j] = ReductionMatrix[i][j + length_required_matrix / 2];
-	}
-
-	for (int i = 0; i < length_required_matrix / 2; i++)
-	{
-		subMatrix3[i] = new int[length_required_matrix / 2];
-		for (int j = 0; j < length_required_matrix / 2; j++)
-			subMatrix3[i][j] = ReductionMatrix[i + length_required_matrix / 2][j];
-	}
-	for (int i = 0; i < length_required_matrix / 2; i++)
-	{
-		subMatrix4[i] = new int[length_required_matrix / 2];
-		for (int j = 0; j < length_required_matrix / 2; j++)
-			subMatrix4[i][j] = ReductionMatrix[i + length_required_matrix / 2][j + length_required_matrix / 2];
-	}
-}
-
-//Function for balancing matrix size (length and width)
-int LengthWidthEqual(int** Result_matrix, int length_or_width, int length_required_matrix) {
-	int x = 0;
-	for (int i = 0; i < length_required_matrix; i++)
-	{
-		x = 0;
-		for (int j = 0; j < length_required_matrix; j++)
+		if (((a == 0 && b == 0 && e == 0 && d != 0 && c == 0) ||
+			(c == 0 && d == 0 && f == 0 && b != 0 && a == 0)))
 		{
-			if (Result_matrix[i][j] != 0)
-			{
-				x++;
-				length_or_width = 100;
-			}
+			double y;
+			if (b == 0)
+				y = f / d;
+			else if (d == 0)
+				y = e / b;
+			else if (e == 0 || f == 0)
+				y = 0;
+			result = "4 " + to_string(y);
 		}
-		if (x == 0 && i < length_or_width)
+		else if (((a == 0 && b == 0 && e == 0 && c != 0 && d == 0) ||
+			(c == 0 && d == 0 && f == 0 && a != 0 && b == 0)))
 		{
-			length_or_width = i;
+			double x;
+			if (a == 0)
+				x = f / c;
+			else if (c == 0)
+				x = e / a;
+			else if (e == 0 || f == 0)
+				x = 0;
+			result = "3 " + to_string(x);
 		}
+		else
+			result = '0';
 	}
-	return length_or_width;
-}
+	else if (a == 0 && c == 0)
+	{
+		double y;
+		if (e == 0)
+			y = f / d;
+		else if (f == 0)
+			y = e / b;
+		else
+			y = e / b;
+		result = "4 " + to_string(y);
+	}
+	else if (b == 0 && d == 0)
+	{
+		double x;
+		if (e == 0)
+			x = f / c;
+		else if (f == 0)
+			x = e / a;
+		else
+			x = e / a;
+		result = "3 " + to_string(x);
+	}
+	else if (b == 0 && e == 0)
+	{
+		double k, n;
+		k = -c / d;
+		n = f / d;
+		result = "1 " + to_string(k) + " " + to_string(n);
+	}
+	else if (d == 0 && f == 0)
+	{
+		double k, n;
+		k = -a / b;
+		n = e / b;
+		result = "1 " + to_string(k) + " " + to_string(n);
+	}
+	else if (a == 0 && e == 0)
+	{
+		double k, n;
+		k = -d / c;
+		n = f / c;
+		result = "1 " + to_string(k) + " " + to_string(n);
+	}
+	else if (c == 0 && f == 0)
+	{
+		double k, n;
+		k = -b / a;
+		n = e / a;
+		result = "1 " + to_string(k) + " " + to_string(n);
+	}
+	else if ((a / b == c / d))
+	{
+		double k, n;
+		k = -c / d;
+		n = f / d;
+		result = "1 " + to_string(k) + " " + to_string(n);
+	}
+	else
+	{
+		result = "Are you kidding me?";
+	}
 
+	return result;
+}
 
 int main()
 {
-	int rows1, columns1, rows2, columns2, switch_counter, length_required_matrix = 2;
-
-	srand(time(NULL));
-
-
-	system("chcp 1251");
-	cout << "You are welcomed by the program" << endl <<
-		"fast matrix multiplication by the Strassen method\n\n";
-
-	//input matrix size
-	do
-	{
-		cout << "Input size of first matrix\n";
-		cin >> rows1 >> columns1;
-	} while (rows1 <= 0 || columns1 <= 0);
-	do
-	{
-		cout << "Input size of second matrix\n";
-		cin >> rows2 >> columns2;
-	} while (rows2 <= 0 || columns2 <= 0);
-
-
-	int** Matrix1 = new int* [rows1];
-	for (int i = 0; i < rows1; i++)
-		Matrix1[i] = new int[columns1];
-	int** Matrix2 = new int* [rows2];
-	for (int i = 0; i < rows2; i++)
-		Matrix2[i] = new int[columns2];
-
-	//Filling matrix with selected method(hand or authomatic)
-	do
-	{
-		cout << "Choose a method for filling in the matrices\n" <<
-			"1 - On hand \n2 - Randomly generated\n";
-		cin >> switch_counter;
-	} while (switch_counter < 1 || switch_counter > 2);
-	switch (switch_counter)
-	{
-	case 1:
-		Matrix1 = InputMatrixOnHand(Matrix1, rows1, columns1);
-		Matrix2 = InputMatrixOnHand(Matrix2, rows2, columns2);
-		cout << "\nMatrix 1\n\n";
-		MatrixOutput(Matrix1, rows1, columns1);
-		cout << "\nMatrix 2\n\n";
-		MatrixOutput(Matrix2, rows2, columns2);
-		break;
-	case 2:
-		Matrix1 = RandomInputMatrix(Matrix1, rows1, columns1);
-		Matrix2 = RandomInputMatrix(Matrix2, rows2, columns2);
-		cout << "\nMatrix 1\n\n";
-		MatrixOutput(Matrix1, rows1, columns1);
-		cout << "\nMatrix 2\n\n";
-		MatrixOutput(Matrix2, rows2, columns2);
-		break;
-	}
-
-	//Reduction matrix to required size
-	while (length_required_matrix < rows1 || length_required_matrix < rows2 || length_required_matrix < columns1 || length_required_matrix < columns2)
-		length_required_matrix *= 2;
-	int** ReductionMatrix1 = new int* [length_required_matrix];
-	for (int i = 0; i < length_required_matrix; i++)
-	{
-		ReductionMatrix1[i] = new int[length_required_matrix];
-		for (int j = 0; j < length_required_matrix; j++)
-			ReductionMatrix1[i][j] = 0;
-	}
-	int** ReductionMatrix2 = new int* [length_required_matrix];
-	for (int i = 0; i < length_required_matrix; i++)
-	{
-		ReductionMatrix2[i] = new int[length_required_matrix];
-		for (int j = 0; j < length_required_matrix; j++)
-			ReductionMatrix2[i][j] = 0;
-	}
-	for (int i = 0; i < rows1; i++)
-	{
-		for (int j = 0; j < columns1; j++)
-			ReductionMatrix1[i][j] = Matrix1[i][j];
-	}
-	for (int i = 0; i < rows2; i++)
-	{
-		for (int j = 0; j < columns2; j++)
-			ReductionMatrix2[i][j] = Matrix2[i][j];
-	}
-	cout << "Reduction matrix\n";
-	cout << "\nMatrix 1\n\n";
-	MatrixOutput(ReductionMatrix1, length_required_matrix, length_required_matrix);
-	cout << "\nMatrix 2\n\n";
-	MatrixOutput(ReductionMatrix2, length_required_matrix, length_required_matrix);
-
-	//Decomposition matrix to submatrix and filling
-
-	int** SubMat1 = new int* [length_required_matrix / 2];
-	int** SubMat2 = new int* [length_required_matrix / 2];
-	int** SubMat3 = new int* [length_required_matrix / 2];
-	int** SubMat4 = new int* [length_required_matrix / 2];
-	int** SubMat5 = new int* [length_required_matrix / 2];
-	int** SubMat6 = new int* [length_required_matrix / 2];
-	int** SubMat7 = new int* [length_required_matrix / 2];
-	int** SubMat8 = new int* [length_required_matrix / 2];
-
-	DecompositiontoSubMatrix(SubMat1, SubMat2, SubMat3, SubMat4, ReductionMatrix1, length_required_matrix);
-	DecompositiontoSubMatrix(SubMat5, SubMat6, SubMat7, SubMat8, ReductionMatrix2, length_required_matrix);
-
-	//Creation intermediate matrices
-	int** intermediate_matrix_1 = new int* [length_required_matrix / 2];
-	int** intermediate_matrix_2 = new int* [length_required_matrix / 2];
-	int** intermediate_matrix_3 = new int* [length_required_matrix / 2];
-	int** intermediate_matrix_4 = new int* [length_required_matrix / 2];
-	int** intermediate_matrix_5 = new int* [length_required_matrix / 2];
-	int** intermediate_matrix_6 = new int* [length_required_matrix / 2];
-	int** intermediate_matrix_7 = new int* [length_required_matrix / 2];
-
-	for (int i = 0; i < length_required_matrix / 2; i++)
-	{
-		intermediate_matrix_1[i] = new int[length_required_matrix / 2];
-		intermediate_matrix_2[i] = new int[length_required_matrix / 2];
-		intermediate_matrix_3[i] = new int[length_required_matrix / 2];
-		intermediate_matrix_4[i] = new int[length_required_matrix / 2];
-		intermediate_matrix_5[i] = new int[length_required_matrix / 2];
-		intermediate_matrix_6[i] = new int[length_required_matrix / 2];
-		intermediate_matrix_7[i] = new int[length_required_matrix / 2];
-	}
-
-	//Calculation values for intermediate matrices
-
-	for (int i = 0; i < length_required_matrix / 2; i++)
-	{
-		for (int j = 0; j < length_required_matrix / 2; j++)
-		{
-
-			intermediate_matrix_1[i][j] = 0;
-			intermediate_matrix_2[i][j] = 0;
-			intermediate_matrix_3[i][j] = 0;
-			intermediate_matrix_4[i][j] = 0;
-			intermediate_matrix_5[i][j] = 0;
-			intermediate_matrix_6[i][j] = 0;
-			intermediate_matrix_7[i][j] = 0;
-
-			for (int z = 0; z < length_required_matrix / 2; z++)
-			{
-				intermediate_matrix_1[i][j] += (SubMat1[i][z] + SubMat4[i][z]) * (SubMat5[z][j] + SubMat8[z][j]);
-				intermediate_matrix_2[i][j] += (SubMat3[i][z] + SubMat4[i][z]) * SubMat5[z][j];
-				intermediate_matrix_3[i][j] += SubMat1[i][z] * (SubMat6[z][j] - SubMat8[z][j]);
-				intermediate_matrix_4[i][j] += SubMat4[i][z] * (SubMat7[z][j] - SubMat5[z][j]);
-				intermediate_matrix_5[i][j] += (SubMat1[i][z] + SubMat2[i][z]) * SubMat8[z][j];
-				intermediate_matrix_6[i][j] += (SubMat3[i][z] - SubMat1[i][z]) * (SubMat5[z][j] + SubMat6[z][j]);
-				intermediate_matrix_7[i][j] += (SubMat2[i][z] - SubMat4[i][z]) * (SubMat7[z][j] + SubMat8[z][j]);
-			}
-
-
-		}
-	}
-
-	//Creation supplementary matrices
-
-	int** SupplementaryMatrix_1 = new int* [length_required_matrix / 2];
-	int** SupplementaryMatrix_2 = new int* [length_required_matrix / 2];
-	int** SupplementaryMatrix_3 = new int* [length_required_matrix / 2];
-	int** SupplementaryMatrix_4 = new int* [length_required_matrix / 2];
-
-	for (int i = 0; i < length_required_matrix / 2; i++)
-	{
-		SupplementaryMatrix_1[i] = new int[length_required_matrix / 2];
-		SupplementaryMatrix_2[i] = new int[length_required_matrix / 2];
-		SupplementaryMatrix_3[i] = new int[length_required_matrix / 2];
-		SupplementaryMatrix_4[i] = new int[length_required_matrix / 2];
-	}
-	//Calculation values of supplementary matrices from intermediate matrices
-
-	for (int i = 0; i < length_required_matrix / 2; i++)
-	{
-		for (int j = 0; j < length_required_matrix / 2; j++)
-		{
-			SupplementaryMatrix_1[i][j] = intermediate_matrix_1[i][j] + intermediate_matrix_4[i][j] - intermediate_matrix_5[i][j] + intermediate_matrix_7[i][j];
-			SupplementaryMatrix_2[i][j] = intermediate_matrix_3[i][j] + intermediate_matrix_5[i][j];
-			SupplementaryMatrix_3[i][j] = intermediate_matrix_2[i][j] + intermediate_matrix_4[i][j];
-			SupplementaryMatrix_4[i][j] = intermediate_matrix_1[i][j] - intermediate_matrix_2[i][j] + intermediate_matrix_3[i][j] + intermediate_matrix_6[i][j];
-		}
-	}
-
-	//Creation resulting matrix
-
-	int** Result_matrix = new int* [length_required_matrix];
-	for (int i = 0; i < length_required_matrix; i++)
-	{
-		Result_matrix[i] = new int[length_required_matrix];
-	}
-
-	//Entering info from supplementary matrices into the resulting matrix
-
-	for (int i = 0; i < length_required_matrix / 2; i++)
-	{
-		for (int j = 0; j < length_required_matrix / 2; j++)
-		{
-			Result_matrix[i][j] = SupplementaryMatrix_1[i][j];
-			Result_matrix[i][j + length_required_matrix / 2] = SupplementaryMatrix_2[i][j];
-			Result_matrix[i + length_required_matrix / 2][j] = SupplementaryMatrix_3[i][j];
-			Result_matrix[i + length_required_matrix / 2][j + length_required_matrix / 2] = SupplementaryMatrix_4[i][j];
-		}
-	}
-
-	// The balancing of boundaries of the matrix
-	int length = 100, width = 100;
-	length = LengthWidthEqual(Result_matrix, length, length_required_matrix);
-	width = LengthWidthEqual(Result_matrix, width, length_required_matrix);
-	int** End_matrix = new int* [length];
-	for (int i = 0; i < length; i++)
-	{
-		End_matrix[i] = new int[width];
-		for (int j = 0; j < width; j++)
-			End_matrix[i][j] = Result_matrix[i][j];
-	}
-
-	//Output resulting matrix
-	cout << "\nResulting matrix\n\n";
-	for (int i = 0; i < length; i++)
-	{
-		for (int j = 0; j < width; j++)
-			cout << End_matrix[i][j] << " ";
-		cout << endl;
-	}
-
-	system("pause");
-
-	//Clearing dynamic memory
-
-	for (int i = 0; i < rows1; i++)
-		delete[] Matrix1[i];
-	for (int i = 0; i < rows2; i++)
-		delete[] Matrix2[i];
-	for (int i = 0; i < length_required_matrix; i++)
-	{
-		delete[] ReductionMatrix1[i];
-		delete[] ReductionMatrix2[i];
-		delete[] Result_matrix[i];
-	}
-	for (int i = 0; i < length; i++)
-		delete[] End_matrix[i];
-	for (int i = 0; i < length_required_matrix / 2; i++)
-	{
-		delete[] SubMat1[i];
-		delete[] SubMat2[i];
-		delete[] SubMat3[i];
-		delete[] SubMat4[i];
-		delete[] SubMat5[i];
-		delete[] SubMat6[i];
-		delete[] SubMat7[i];
-		delete[] SubMat8[i];
-		delete[] SupplementaryMatrix_1[i];
-		delete[] SupplementaryMatrix_2[i];
-		delete[] SupplementaryMatrix_3[i];
-		delete[] SupplementaryMatrix_4[i];
-		delete[] intermediate_matrix_1[i];
-		delete[] intermediate_matrix_2[i];
-		delete[] intermediate_matrix_3[i];
-		delete[] intermediate_matrix_4[i];
-		delete[] intermediate_matrix_5[i];
-		delete[] intermediate_matrix_6[i];
-		delete[] intermediate_matrix_7[i];
-	}
-	delete[]Matrix1, Matrix1, ReductionMatrix1, ReductionMatrix2, Result_matrix, End_matrix;
-	delete[] SupplementaryMatrix_1, SupplementaryMatrix_2, SupplementaryMatrix_3, SupplementaryMatrix_4,
-		delete[] SubMat1, SubMat2, SubMat3, SubMat4, SubMat5, SubMat6, SubMat7, SubMat8;
-	delete[] intermediate_matrix_1, intermediate_matrix_2, intermediate_matrix_3, intermediate_matrix_4, intermediate_matrix_5, intermediate_matrix_6, intermediate_matrix_7;
+	double a, b, c, d, e, f;
+	cin >> a >> b >> c >> d >> e >> f;
+	cout << Program(a, b, c, d, e, f);
 	return 0;
 }
---
+++ UnitTest1.cpp
@@ -0,0 +1,121 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+#include "C:\\Users\\8181049\\source\\repos\\pppp_2\\pppp_2\\Source.cpp"
+
+using namespace std;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(Main_test_TEST1)
+		{
+			Assert::IsTrue(Program(0, 0, 0, 0, 0, 0) == "5", L"Assert");
+		}
+
+		TEST_METHOD(Main_test_TEST2)
+		{
+			Assert::IsTrue(Program(0, 1, 0, 0, 1, 0) == "4 " + to_string(1.0) , L"Assert");
+		}
+
+		TEST_METHOD(Main_test_TEST3)
+		{
+			Assert::IsTrue(Program(0, 0, 0, 1, 0, 1) == "4 " + to_string(1.0), L"Assert");
+		}
+
+		TEST_METHOD(Main_test_TEST4)
+		{
+			Assert::IsTrue(Program(0, 1, 0, 0, 1, 0) == "4 " + to_string(1.0), L"Assert");
+		}
+
+		TEST_METHOD(Main_test_TEST5)
+		{
+			Assert::IsTrue(Program(0, 0, 0, 1, 0, 0) == "4 " + to_string(0.0), L"Assert");
+		}
+
+		TEST_METHOD(Main_test_TEST6)
+		{
+			Assert::IsTrue(Program(0, 0, 1, 0, 0, 0) == "3 " + to_string(0.0), L"Assert");
+		}
+
+		TEST_METHOD(Main_test_TEST7)
+		{
+			Assert::IsTrue(Program(1, 0, 0, 0, 5, 0) == "3 " + to_string(5.0), L"Assert");
+		}
+
+		TEST_METHOD(Main_test_TEST8)
+		{
+			Assert::IsTrue(Program(1, 0, 1, 0, 0, 0) == "3 " + to_string(0.0), L"Assert");
+		}
+
+		TEST_METHOD(Main_test_TEST9)
+		{
+			Assert::IsTrue(Program(0, 0, 0, 0, 1, 1) == "0", L"Assert");
+		}
+
+		TEST_METHOD(Main_test_TEST10)
+		{
+			Assert::IsTrue(Program(0, 0, 0, 1, 0, 1) == "4 " + to_string(1.0), L"Assert");
+		}
+
+		TEST_METHOD(Main_test_TEST11)
+		{
+			Assert::IsTrue(Program(0, 1, 0, 0, 3, 0) == "4 " + to_string(3.0), L"Assert");
+		}
+
+		TEST_METHOD(Main_test_TEST12)
+		{
+			Assert::IsTrue(Program(0, 1, 0, 1, 3, 3) == "4 " + to_string(3.0), L"Assert");
+		}
+
+		TEST_METHOD(Main_test_TEST13)
+		{
+			Assert::IsTrue(Program(0, 0, 1, 0, 0, 11) == "3 " + to_string(11.0), L"Assert");
+		}
+
+		TEST_METHOD(Main_test_TEST14)
+		{
+			Assert::IsTrue(Program(11, 0, 0, 0, 121, 0) == "3 " + to_string(11.0), L"Assert");
+		}
+
+		TEST_METHOD(Main_test_TEST15)
+		{
+			Assert::IsTrue(Program(8, 0, 4, 0, 4, 2) == "3 " + to_string(0.5), L"Assert");
+		}
+
+		TEST_METHOD(Main_test_TEST16)
+		{
+			Assert::IsTrue(Program(1, 0, 1, 1, 0, 0) == "1 " + to_string(-1.0) + " " + to_string(0.0), L"Assert");
+		}
+
+		TEST_METHOD(Main_test_TEST17)
+		{
+			Assert::IsTrue(Program(1, 1, 1, 0, 0, 0) == "1 " + to_string(-1.0) + " " + to_string(0.0), L"Assert");
+		}
+		TEST_METHOD(Main_test_TEST18)
+		{
+			Assert::IsTrue(Program(0, 2, 20, 1, 0, 0) == "1 " + to_string(-0.05) + " " + to_string(0.0), L"Assert");
+		}
+
+		TEST_METHOD(Main_test_TEST19)
+		{
+			Assert::IsTrue(Program(10, 10, 0, 2, 0, 0) == "1 " + to_string(-1.0) + " " + to_string(0.0), L"Assert");
+		}
+
+		TEST_METHOD(Main_test_TEST20)
+		{
+			Assert::IsTrue(Program(20, 20, 10, 10, 0, 0) == "1 " + to_string(-1.0) + " " + to_string(0.0), L"Assert");
+		}
+
+		TEST_METHOD(Main_test_TEST21)
+		{
+			Assert::IsTrue(Program(20, 20, 10, 9, 0, 0) == "Are you kidding me?");
+		}
+
+	};
+}
